9dca2182eeb0c3e901a819402df3e42e1415e6f7
JeanPhilippeSeguin <jp_seg@outlook.com>
2026-02-14 12:18:44 -0500

    add logging for session serializer, only login if canActivate is true

--- api/src/modules/auth/auth.guard.ts
+++ api/src/modules/auth/auth.guard.ts
@@ -6,10 +6,14 @@ import { Request } from 'express';
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
   async canActivate(context: ExecutionContext) {
-    const result = (await super.canActivate(context)) as boolean;
-    const request = context.switchToHttp().getRequest<Request>();
-    await super.logIn(request);
-    return result;
+    const canActivate = (await super.canActivate(context)) as boolean;
+
+    if (canActivate) {
+      const request = context.switchToHttp().getRequest<Request>();
+      await super.logIn(request);
+    }
+
+    return canActivate;
   }
 }
 
--- api/src/modules/auth/auth.serializer.ts
+++ api/src/modules/auth/auth.serializer.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { PassportSerializer } from '@nestjs/passport';
 
 import { UserService } from '../user/user.service';
 import { UserEntity } from '../user/user.entity';
 
-type PassportSerializerCallback<T> = (err: null, user: T | null) => void;
+type PassportSerializerCallback<T> = (
+  err: Error | null,
+  user: T | null,
+) => void;
 
 @Injectable()
 export class SessionSerializer extends PassportSerializer {
+  private readonly logger: Logger = new Logger(SessionSerializer.name);
+
   constructor(private readonly userService: UserService) {
     super();
   }
@@ -21,17 +26,18 @@ export class SessionSerializer extends PassportSerializer {
 
   async deserializeUser(
     userUUID: string,
-    cb: PassportSerializerCallback<UserEntity>,
+    cb: PassportSerializerCallback<Express.User>,
   ): Promise<void> {
     let user: UserEntity | null = null;
 
     try {
       user =
         (await this.userService.findActiveUserByUserUUID(userUUID)) || null;
-    } catch {
+    } catch (exception) {
+      this.logger.error(exception);
       user = null;
     }
 
-    return cb(null, user);
+    return cb(null, user?.uuid ? { uuid: user.uuid } : null);
   }
 }
